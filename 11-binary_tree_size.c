7cdae0bf15c7fe12e92fa2c8b95bd02a81262944
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-29 09:57:18 -0800

    11. Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,7 +4,7 @@
  * binary_tree_size - measures the size of a binary tree
  * @tree: pointer to the root node of the tree to count the
  * number of leaves
- * 
+ *
  * Return: 0 if the tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
