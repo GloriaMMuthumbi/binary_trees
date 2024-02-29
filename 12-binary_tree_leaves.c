b6e8c783ff516f36707bf818d4904cfc69d87e8c
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-29 11:22:24 -0800

    12. Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves
- * 
+ *
  * Return: 0 if tree is NULL, otherwise count of the leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
